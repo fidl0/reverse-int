bf24b605d46f2f2896908e03d43757de1ea6afd5
Alexander Fidlovskiy <afidlovskiy@gmail.com>
2022-06-24 15:13:54 +0300

    fix: change consequence of return

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse (n) {
   result = '';
   for (let i = 0; i < n.length; i++) {
-    result = `${result}${n[i]}`
+    result = `${n[i]}${result}`;
   }
   return result;
 }
